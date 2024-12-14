bb3cd57e614bf2643624d669ac9b6fb49e89de98
PanJ <panjmp@gmail.com>
2024-12-14 18:25:22 +0700

    fix avatar color algo

--- packages/Webkul/Admin/src/Resources/views/components/avatar/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/avatar/index.blade.php
@@ -56,7 +56,17 @@
 
             methods: {
                 colorClasses() {
-                    let index = Math.floor(Math.random() * this.colors.background.length);
+                    function stringToHash(string) {
+                        let hash = 0;
+                        if (string.length == 0) return hash;
+                        for (let i = 0; i < string.length; i++) {
+                            const char = string.charCodeAt(i);
+                            hash = ((hash << 5) - hash) + char;
+                            hash = hash & hash;
+                        }
+                        return hash;
+                    }
+                    let index = Math.abs(stringToHash(this.name)) % this.colors.background.length
 
                     return [
                         this.colors.background[index],
